b6e67a9d1e1eb3ba3837a0ffea546083299ef2a4
donib-irakihda <donib.irakihda@gmail.com>
2023-05-22 13:50:24 +0545

    add: api logic

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.get("/convert", async (req, res) => {
+  const { amount, currencyFrom, currencyTo } = req.query;
+  const apiUrl = `https://api.exchangerate-api.com/v4/latest/${currencyFrom}`;
+  try {
+    const response = await axios.get(apiUrl);
+    const exchangeRates = response.data.rates;
+    const result = amount * exchangeRates[currencyTo];
+    console.log("rate: ", exchangeRates[currencyTo])
+    res.json({ result });
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
